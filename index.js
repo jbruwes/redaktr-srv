031ff6e4d186136cada8f0565fe2ff291e1b3630
Jerry Bruwes <jbruwes@icloud.com>
2020-08-30 19:16:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-'use strict'
-let aws = require('aws-sdk');
-let s3 = new aws.S3({
-    accessKeyId: process.env.NODE_MINIO_ACCESS_KEY,
-    secretAccessKey: process.env.NODE_MINIO_SECRET_ACCESS_KEY,
-    endpoint: "http://s3.redaktr",
-    s3ForcePathStyle: true,
-    signatureVersion: "v4"
-});
-let http = require('http');
-let html = require('./html');
-let kharon = require('./kharon');
-let hermes = require('./hermes');
-http
-  .createServer((req, res) => {
-    req.on('data', chunk => {
-      const event = JSON.parse(chunk);
-      const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-      let id = key.match(/^[^\/]+/)[0];
-      id = id.substr(0, id.lastIndexOf('.')) || id;
-      const contentType = event.Records[0].s3.object.userMetadata["content-type"].match(/^.*(?=;)/)[0];
-      console.log("content-type =", contentType);
-      console.log("key =", key);
-      console.log("id =", id);
-	Promise.all([s3.getObject({Bucket: 'redaktr', Key: 'redaktr.json'}).promise(),
-	s3.getObject({
-		Bucket: 'redaktr',
-		Key: id + '.json',
-	}).promise(), s3.getObject({
-		Bucket: 'redaktr',
-		Key: id + '.html'
-	}).promise()]).then(value => {
-		let buf = JSON.parse(value[0].Body)[0];
-		value[0] = {};
-		value[0].Item = buf;
-		console.log(value);
-		switch(contentType) {
-			case "application/json":
-			kharon.main(value, id, key, s3, _ => {console.log('kharon done.')});
-			break;
-			case "text/html":
-			if(id + '.html' === key)hermes.main(value, id, key, s3, _ => {console.log('hermes done.')});
-			else html.main(value, id, key, s3, _ => {console.log('html done.')});
-			break;
-		}
-	}).catch(err => {
-		console.log(err);
-	});
-    });
-    res.end();
-  })
-  .listen(3000);
+module.exports = {
+        main: (name, tree, s3, f_sitemap, f_sitemap2) => {
+                var callback = _ => {
+                        f_sitemap(tree, '');
+                        if (f_sitemap2) f_sitemap2();
+                };
+                Promise.all([s3.listObjects({
+                        Bucket: 'redaktr.com',
+                        Prefix: name + '/'
+                }).promise(), s3.listObjects({
+                        Bucket: 'm.redaktr.com',
+                        Prefix: name + '/'
+                }).promise()]).then(values => {
+                        var deleter = (data, params) => {
+                                        data.Contents.forEach(content => {
+                                                if (!content.Key.match(/^[^\/]+\/?[^\/]+$/)) params.Delete.Objects.push({
+                                                        Key: content.Key
+                                                });
+                                        });
+                                        return params.Delete.Objects.length ? s3.deleteObjects(params).promise() : null;
+                                },
+                                param1 = {
+                                        Bucket: 'redaktr.com',
+                                        Delete: {
+                                                Objects: []
+                                        }
+                                },
+                                param2 = {
+                                        Bucket: 'm.redaktr.com',
+                                        Delete: {
+                                                Objects: []
+                                        }
+                                };
+                        if (values[0].Contents.length === 0 && values[1].Contents.length === 0) callback();
+                        else Promise.all([deleter(values[0], param1), deleter(values[1], param2)]).then(dvalues => {
+                                if (dvalues[0].Deleted.length === 1000 || dvalues[1].Deleted.length === 1000) module.exports.main(name, tree, s3, f_sitemap, f_sitemap2);
+                                else callback();
+                        }).catch(err => {
+                                console.log('deleteFolder.js', name);
+                                console.log('s3.deleteObjects', JSON.stringify(param1));
+                                console.log('s3.deleteObjects', JSON.stringify(param2));
+                                console.log(JSON.stringify(err));
+                                callback(err);
+                        });
+                }).catch(err => {
+                        console.log('deleteFolder.js', name);
+                        console.log('s3.listObjects', {
+                                Bucket: 'redaktr.com',
+                                Prefix: name + '/'
+                        });
+                        console.log('s3.listObjects', {
+                                Bucket: 'm.redaktr.com',
+                                Prefix: name + '/'
+                        });
+                        console.log(JSON.stringify(err));
+                        callback(err);
+                });
+        }
+};
